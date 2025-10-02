cc9079de1d831e18f2eee74dc148566ce19ab642
devrisby <131298724+rezbee-dev@users.noreply.github.com>
2025-10-02 19:23:37 -0400

    save changes weekly-1-noodles

--- week-1/noods/src/lib/components/HeroSection.svelte
+++ week-1/noods/src/lib/components/HeroSection.svelte
@@ -1,9 +1,65 @@
-<main class="hero">
+<script lang="ts">
 
+</script>
+
+<main class="hero">
+    <div class="hero-img">
+        <div class="hero-content">
+            <h1>GOSHEN<br>NOODLES</h1>
+            <h2>Discover the authentic taste of Vietnam.</h2>
+        </div>
+    </div>
 </main>
 
-<script>
+<style>
+
     .hero {
-        
+        height: 100vh;
+        color: #fff;
     }
-</script>
+
+    .hero-img {
+        background-image: url("$lib/assets/hero-img.png");
+        background-color: #cccccc;
+        background-size: cover;
+        background-position: center;
+        height: 100%;
+        position: relative;
+    }
+
+    .hero-img::before {
+        content: "";
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: rgba(71,71,71,.7);
+        z-index: 1;
+    }
+
+    .hero-content {
+        z-index: 2;
+        position: relative;
+        font-family: "Afacad", sans-serif;
+        height: 100%;
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .hero-content h1 {
+        font-weight: 100;
+        font-size: 8rem;
+        line-height: 1.1;
+        text-align: center;
+    }
+
+    .hero-content h2 {
+        font-weight: 100;
+        font-size: 2em;
+        text-align: center;
+    }
+</style>
--- week-1/noods/src/routes/+layout.svelte
+++ week-1/noods/src/routes/+layout.svelte
@@ -5,7 +5,10 @@
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
 <nav class="navbar">
 	<ul>
 		<li>About</li>
@@ -22,16 +25,26 @@
 		top: 0;
 		text-align: center;
 		background-color: rgb(251 191 36);
-		padding: 2em 0;
+		padding: 1.5em 0;
+		font-family: "Inter", sans-serif;
+		font-optical-sizing: auto;
+		font-weight: 400;
+		font-style: normal;
+		color: #fff;
 	}
 
 	.navbar ul {
 		list-style-type: none;
+		padding: 0;
 	}
 
 	.navbar ul li {
 		display: inline-block;
-		padding: 0 1em;
+		padding-right: 2.5em;
 	}
+/* 
+	.navbar ul li:nth-child(n+1) {
+		padding: 0 1.5em;
+	} */
 
 </style>
