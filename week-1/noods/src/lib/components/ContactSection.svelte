6c934a06a2eb1e45898c159d7fa3f781b554753f
devrisby <118779687+devrisby@users.noreply.github.com>
2025-10-13 14:07:41 -0400

    save progress on contact form

--- week-1/noods/src/lib/components/ContactSection.svelte
+++ week-1/noods/src/lib/components/ContactSection.svelte
@@ -1,8 +1,17 @@
-<div class="container">
-    <div class="contact">
+<section class="container">
+    <article class="contact">
+        <h2>Restaurant Information:</h2>
+        <p>1303 College</p>
+        <p>Goshen, IN 46526</p>
 
-    </div>
-    <div class="map">
-        
-    </div>
-</div>
+        <h2>Contact:</h2>
+        <p>Phone: (574) 534-1793</p>
+        <p>Hours:</p>
+        <p>Mon: 11am - 7pm</p>
+        <p>Tuesday: Closed.</p>
+        <p>Wed-Sun: 11am - 7pm</p>
+    </article>
+    <article class="map">
+        <p>I'm a map</p>        
+    </article>
+</section>
--- week-1/noods/src/routes/+page.svelte
+++ week-1/noods/src/routes/+page.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
     import HeroSection from "$lib/components/HeroSection.svelte";
     import CarouselFood from "$lib/components/CarouselFood.svelte";
+	import ContactSection from "$lib/components/ContactSection.svelte";
 
     console.log("Hello World!")
 </script>
 
 <HeroSection />
 <CarouselFood />
+<ContactSection />
